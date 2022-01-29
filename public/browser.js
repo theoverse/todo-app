fa8f42f40382d3f056d97042eb4cafc9ed13550b
theoverse <theoverse@protonmail.com>
2022-01-29 14:12:11 -0800

    added delete operation

--- public/browser.js
+++ public/browser.js
@@ -1,4 +1,16 @@
 document.addEventListener("click", function(event) {
+    // delete feature
+    if(event.target.classList.contains("delete-me")) {
+        if (confirm("Do you want to delete this item?")) {
+            axios.post('/delete-item', {id: event.target.getAttribute("data-id")}).then(function() {
+                event.target.parentElement.parentElement.remove()
+            }).catch(function() {
+                console.log("Something went wrong!")
+            })
+        }
+    }
+    
+    // update feature
     if (event.target.classList.contains("edit-me")) {
         let userInput = prompt("Enter new to-do:", event.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
         if (userInput) {
