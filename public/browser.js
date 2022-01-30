1c581769ead2a415803d0b3b3c82b32ab8a8c046
theoverse <theoverse@protonmail.com>
2022-01-30 08:49:09 -0800

    fixed click and item submit duplication bug

--- public/browser.js
+++ public/browser.js
@@ -1,35 +1,35 @@
-document.addEventListener("click", function(event) {
-    function itemTemplate(item) {
-        return ` <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-        <span class="item-text">${item.text}</span>
-        <div>
-          <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-          <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
-        </div>
-      </li>`
-    }
+function itemTemplate(item) {
+    return ` <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+    <span class="item-text">${item.text}</span>
+    <div>
+      <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+      <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+    </div>
+  </li>`
+}
 
-    // initial page load render
-    let ourHTML = items.map(function(item) {
-        return itemTemplate(item)
-    }).join('')
-    document.getElementById("item-list").insertAdjacentHTML("beforeend", ourHTML)
+// initial page load render
+let ourHTML = items.map(function(item) {
+    return itemTemplate(item)
+}).join('')
+document.getElementById("item-list").insertAdjacentHTML("beforeend", ourHTML)
 
-    // create feature
-    let createField = document.getElementById("create-field")
+// create feature
+let createField = document.getElementById("create-field")
 
-    document.getElementById("create-form").addEventListener("submit", function(event) {
-        event.preventDefault()
-        axios.post('/create-item', {text: createField.value}).then(function(response) {
-            // create the html for a new item
-            document.getElementById("item-list").insertAdjacentHTML("beforeend", itemTemplate(response.data))
-            createField.value = ""
-            createField.focus()
-        }).catch(function() {
-            console.log("Something went wrong!")
-        })
+document.getElementById("create-form").addEventListener("submit", function(event) {
+    event.preventDefault()
+    axios.post('/create-item', {text: createField.value}).then(function(response) {
+        // create the html for a new item
+        document.getElementById("item-list").insertAdjacentHTML("beforeend", itemTemplate(response.data))
+        createField.value = ""
+        createField.focus()
+    }).catch(function() {
+        console.log("Something went wrong!")
     })
+})
 
+document.addEventListener("click", function(event) {
     // delete feature
     if(event.target.classList.contains("delete-me")) {
         if (confirm("Do you want to delete this item?")) {
